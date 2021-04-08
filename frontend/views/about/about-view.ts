d40abab81d2dd6fcad36f3bf2360b5cb21a5c0f5
Marcus Hellberg <marcus@vaadin.com>
2021-04-08 14:58:06 -0700

    final

--- frontend/views/about/about-view.ts
+++ frontend/views/about/about-view.ts
@@ -8,11 +8,25 @@ import "@vaadin/vaadin-grid/vaadin-grid-column";
 
 @customElement("about-view")
 export class AboutView extends View {
+  connectedCallback() {
+    super.connectedCallback();
+    this.classList.add("flex", "flex-col", "spacing-b-m", "h-full");
+  }
+
   render() {
     return html`
-      <marquee>
-        <h1>Layouting in Vaadin Fusion</h1>
-      </marquee>
+      <div class="flex items-end spacing-e-m">
+        <vaadin-text-field label="Name"></vaadin-text-field>
+        <vaadin-button>Click me</vaadin-button>
+      </div>
+
+      <vaadin-grid class="flex-grow">
+        <vaadin-grid-column header="Column 1"></vaadin-grid-column>
+        <vaadin-grid-column header="Column 2"></vaadin-grid-column>
+        <vaadin-grid-column header="Column 3"></vaadin-grid-column>
+        <vaadin-grid-column header="Column 4"></vaadin-grid-column>
+        <vaadin-grid-column header="Column 5"></vaadin-grid-column>
+      </vaadin-grid>
     `;
   }
 }
